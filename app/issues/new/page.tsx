3ff182367fab377d216f67dfc0f0ca02295b8358
abarmpas <57562538+abarmpas@users.noreply.github.com>
2023-11-21 01:25:06 +0200

    POST issues with axios and react-hook form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,17 +2,38 @@
 
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import { Button, TextArea, TextFieldInput, TextFieldRoot } from "@radix-ui/themes"
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+    title: string,
+    description: string
+}
 
 const NewIssuePage = () => {
+    const router = useRouter();
+    const {register, control, handleSubmit} = useForm<IssueForm>();
+
     return (
-        <div className="max-w-xl space-y-3">
+        <form 
+            className="max-w-xl space-y-3" 
+            onSubmit={handleSubmit(async (data) => {
+                await axios.post('/api/issues', data);
+                router.push('/issues');
+            })}>
             <TextFieldRoot>
-                <TextFieldInput placeholder="Title" />
+                <TextFieldInput placeholder="Title" {...register('title')}/>
             </TextFieldRoot>
-            <SimpleMDE placeholder="Description" />
+            <Controller 
+                name="description"
+                control={control}
+                render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+            />
+            
             <Button>Submit New Issue</Button>
-        </div>
+        </form>
     )
 }
 
